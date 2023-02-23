5815b3276a91053a35565b48f476014be7b65822
KonstantinYev <109310447+KonstantinYevchuk@users.noreply.github.com>
2023-02-23 20:11:34 +0200

    change reset

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -16,7 +16,7 @@ export class Form extends Component {
       handleSubmit = e => {
         e.preventDefault();
         this.props.submit(this.state)
-        e.currentTarget.reset()
+        // e.currentTarget.reset()
         this.reset()
         }
       reset = () => {
@@ -30,6 +30,7 @@ export class Form extends Component {
                <Input 
                type="text"
                name="name"
+               value={this.state.name}
                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                required
@@ -39,6 +40,7 @@ export class Form extends Component {
               <Input
               type="tel"
               name="number"
+              value={this.state.number}
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
